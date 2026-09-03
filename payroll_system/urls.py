7a63a8753b7c3843e9bb6aabc63a0a9597cd1657
proinsight25-art <proinsight25@gmail.com>
2026-09-03 15:10:56 +0200

    Update urls.py

--- payroll_system/urls.py
+++ payroll_system/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Pro Insight Payroll Solutions!")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),       # Root URL
+    path('admin/', admin.site.urls),   # Admin
 ]
