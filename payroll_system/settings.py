30600e2f8c1b09ac8c0b6660953267f13ef356a8
proinsight25-art <proinsight25@gmail.com>
2026-09-04 09:10:58 +0200

    Update settings.py

--- payroll_system/settings.py
+++ payroll_system/settings.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = 'replace-this-with-a-secret-key'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
@@ -22,15 +27,35 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'payroll_system.urls'
-
 WSGI_APPLICATION = 'payroll_system.wsgi.application'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Templates configuration so Django finds templates/registration/login.html
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ BASE_DIR / 'templates' ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Login redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Harare'
